36319b61651444a429266b2c13e880eb99377bbf
Baptiste Leproux <leproux.baptiste@gmail.com>
2023-05-30 16:52:59 +0200

    Update tokens.config.ts

--- tokens.config.ts
+++ tokens.config.ts
@@ -1,27 +1,9 @@
 import { defineTheme } from 'pinceau'
 
 export default defineTheme({
-  alpine: {
-    body: {
-      color: {
-        value: {
-          dark: "white"
-        }
-      }
-    }
-  },
-
   color: {
     green: {
-      100: {
-        value: "#5d746a"
-      }
-    },
-
-    primary: {
-      500: {
-        value: "#1aff34"
-      }
+      100: "#5d746a"
     }
   }
 })
